57964790d5dae6f90e77ebfe23991608c5912a29
Tay Araujo <132668816+TaizaArauj0@users.noreply.github.com>
2023-07-06 13:38:28 +0000

    Salvar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-Alert("Olá!")
+function carregar() {
+     var msg = window.document.getElementById("msg") 
+     var img = window.document.getElementById("Imagem")
+     var data = new Date()
+     var hora = data.getHours()
+     msg.innerHTML = `Agora são ${hora} horas.`
+     if (hora > 0 && hora < 12) {
+         // BOM DIA! 
+         img.src = 'manha.png'
+         document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora <=18) { 
+        // BOA TARDE!
+         img.src = 'tarde.png'
+         document.body.style.background='orange'
+    } else {
+         // BOA NOITE!
+         img.src = 'noite.png'
+         document.body.style.background='blue'
+         
+    }
+}
